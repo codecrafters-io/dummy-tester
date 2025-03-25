2dbf3c75366663235b07be388ada47f8d8daeb4d
Ryan Gang <ryan-gg@outlook.com>
2025-03-25 12:34:02 +0530

    Update test case slug in stages_test.go from "yz1" to "ah7"

--- internal/stages_test.go
+++ internal/stages_test.go
@@ -9,7 +9,7 @@ import (
 func TestStages(t *testing.T) {
 	testCases := map[string]tester_utils_testing.TesterOutputTestCase{
 		"init_failure": {
-			StageSlugs:          []string{"yz1"},
+			StageSlugs:          []string{"ah7"},
 			CodePath:            "./test_helpers/scenarios/init/failure",
 			ExpectedExitCode:    1,
 			StdoutFixturePath:   "./test_helpers/fixtures/init/failure",
